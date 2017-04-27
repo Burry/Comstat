187363ab7a1f523138ec98b7bb3d32dc2b04be66
Grant Burry <grant@burry.me>
2017-04-27 14:00:18 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Comstat
+-------
+Custom front-end to track Comcast data usage, allowing you to host a simple node.js server to share with roommates/family. Powered by Eric Swanson's [python script](comcast).
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var childProcess = require('child_process').spawn;
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Data Meter'
+    });
+});
+
+router.get('/response', function(req, res, next) {
+    var username = req.app.get('username');
+    var password = req.app.get('password');
+    var comcastPy = childProcess('python3',[
+        'comcast.py',
+        username,
+        password
+    ]);
+    comcastPy.stdout.pipe(res);
+});
+
+module.exports = router;
