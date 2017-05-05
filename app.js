d307ee3ccc13ddc75ef02ebbf0565e03d0e4851b
Grant Burry <grant@burry.me>
2017-05-05 00:08:28 -0600

    Cleaned up folder structure

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./modules/index');
+var index = require('./lib/index');
 
 var app = express();
 
