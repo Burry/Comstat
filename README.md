e6d73b55a55a5397f11e46d8bff46729b206e42a
Grant Burry <grant@burry.me>
2017-05-07 21:33:57 -0600

    Big update - new db, working graph
    Got the daily use graph working. Required switching from TingoDB to the
    full MongoDB so data can be aggregated. Real-time data loading across
    all clients with Socket.io working well.

--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 
 Comstat
 -------
-Comcast forces you to go through a pretty crappy login process until they finally show you your data usage on a simple progress bar. This project uses Eric Swanson's [Python script](https://github.com/lachesis/comcast) to fetch the information from Comcast and then presents it in a nice front-end which...
-    - You can host and share with your family or roommates
-    - Requires you to authenticate exactly once
-    - Can be easily expanded upon in the future with statistics and use predictions to make something much more useful than what Comcast provides
+Comcast forces you to go through a pretty crappy login process until they finally show you your data usage on a simple progress bar. This project uses Eric Swanson's [Python script](https://github.com/lachesis/comcast) to fetch the information from Comcast and then presents it in a front-end for you.
+
+Features
+-------
+    - Log in only once
+    - Auto-loads usage data in background so you don't have to wait to check it
+    - Tracks usage for the month to display a nice graph
+    - Slick, mobile-friendly UI
+    - Ideal for proxying through something like nginx and hosting for your family/roommates
+    - Planned: add detailed statistics and use predictions
 
 Preview
 -------
@@ -17,12 +23,11 @@ Installation
 Requires Node.js and Python 3 with the requests library.
 1. [Download](https://github.com/Burry/Comstat/archive/master.zip) the repo or run `git clone https://github.com/Burry/Comstat.git`
 2. Inside the Comstat directory, run `sudo npm install` to install node dependencies.
+3. Create the database directory in your root folder with `sudo mkdir -p /data/db && sudo chown -R $USER /data/db`. If you'd like, you can use a different directory, but then you must ensure that Mongo has permissions to it, and you must update the path in the pre- and post- start scripts in package.json.
 
 Usage
 -------
 Enter the Comstat directory and start the server with `npm start`
-
-If you see an `EACCES: permission denied` error, you may need to run `sudo chown -R [YOURUSERNAME] db` first.
 
 You should then be able to access Comstat at [http://localhost:3234]().
 
--- public/javascripts/comstat.js
+++ public/javascripts/comstat.js
@@ -7,10 +7,10 @@ socket.on('notifyComcastQuery', function() {
 });
 
 socket.on('updateComcastQuery', function(data) {
-    updateStats(data);
     $(".loader").hide();
     $(".fa-refresh").removeClass("fa-spin");
     $("#refresh").prop("disabled",false);
+    updateStats(data);
 });
 
 socket.on('updateComcastQueryStatus', function(data) {
@@ -28,20 +28,6 @@ function updateStats(data) {
     var alertOptions;
     var percent = 100 * data.totalUsed / dataCap;
     var percentInt = Math.round(percent);
-    var progressBarClasses = $(".progress-bar").attr("class").split(/\s+/);
-    var progressBarBackground;
-    if (percentInt >= 50 && percentInt < 70) {
-        progressBarBackground = 'bg-success';
-    } else if (percentInt >= 70 && percentInt < 90) {
-        progressBarBackground = 'bg-warning';
-    } else if (percentInt >= 90) {
-        progressBarBackground = 'bg-danger';
-    }
-    $.each(progressBarClasses, function(index, item) {
-        if (item.startsWith('bg-')) {
-            $(".progress-bar").removeClass(item);
-        }
-    });
     if (percent >= 100) {
         percent = 100;
         var overageCost = Math.ceil((data.totalUsed - dataCap + 1) / 50) * 10;
@@ -68,20 +54,105 @@ function updateStats(data) {
     $("#data_overage_charges").html(alertOptions.charges);
     $("#data_overage_charges_row").css("display", alertOptions.chargesDisplay);
     $("#days_remaining").html(daysRemaining());
-    $(".progress-bar").css("width", percent + "%");
-    $(".progress-bar").addClass(progressBarBackground);
 }
 
+function gimme30() {
+    var array = [];
+    var m = new Date().getMonth();
+	var y = new Date().getYear();
+	var daysInMonth = new Date(y, m, 0).getDate();
+    for (var i = 1; i <= daysInMonth; i++) {
+        array.push(m + '/' + i)
+    }
+    return array;
+}
+
+function setChartSize() {
+	var size = [$(".jumbotron-fluid .container").css("width"), $(".jumbotron-fluid").css("height")]
+	if ($(window).width() >= 576) size[0] = parseInt(size[0].slice(0, -2)) - 30 + 'px';
+	size[1] = parseInt(size[1].slice(0, -2)) + 10 + 'px';
+	$(".chart-container").width(size[0]);
+	$(".chart-container").height(size[1]);
+}
+
+$(document).ready(function() {
+    requestComcastQuery()
+
+	var cumulativeUse = [];
+	var recordedDate = j = dailyUsageData[0]._id - 2;
+	for (var i = 0; i < recordedDate; i++) {
+		cumulativeUse[i] = 0;
+		if (dailyUsageData[i]) cumulativeUse[j] = dailyUsageData[i].totalUsed;
+		j++;
+	}
+
+    var data = {
+        labels: gimme30(),
+        datasets: [{
+            type: 'line',
+            fill: true,
+            backgroundColor: 'rgb(215, 19, 40)',
+            borderColor: 'rgb(215, 19, 40)',
+            borderWidth: 2,
+            data: cumulativeUse
+        }]
+    };
+
+    var options = {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    display: false,
+                    beginAtZero:true,
+                    max: 1024
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+		maintainAspectRatio: false
+    };
+
+	setChartSize();
+
+    var ctx = $("#chart");
+
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+});
+
 function requestComcastQuery() {
-    socket.emit('requestComcastQuery', 'test', function(data) {
+    socket.emit('requestComcastQuery', null, function(data) {
         updateStats(data);
     });
 }
 
-$("#refresh").click(function() {
-    requestComcastQuery();
+$(window).resize(function() {
+	setChartSize();
 });
 
-$(document).ready(function() {
-    requestComcastQuery()
+$("#refresh").click(function() {
+    requestComcastQuery();
 });
